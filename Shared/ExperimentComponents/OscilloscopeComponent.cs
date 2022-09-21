18c5f652c000bd44ecb256333823670c07a52e0c
Stephan Troyer <stephantroyer@live.at>
2022-09-21 16:05:46 +0200

    Oscilloscope: Add waveform time

--- Shared/ExperimentComponents/OscilloscopeComponent.cs
+++ Shared/ExperimentComponents/OscilloscopeComponent.cs
@@ -4,7 +4,7 @@ namespace ExperimentFramework;
 [IconString("\xE9D9")]
 public abstract class OscilloscopeComponent : ExperimentComponentClass
 {
-    public abstract float[] GetWaveform(string channel);
+    public abstract (float, float)[] GetWaveform(string channel);
 }
 
 public class FakeOscilloscopeSettings
@@ -16,13 +16,13 @@ public class FakeOscilloscope : OscilloscopeComponent
 {
     public FakeOscilloscope(FakeOscilloscopeSettings settings) { }
 
-    public override float[] GetWaveform(string channel)
+    public override (float, float)[] GetWaveform(string channel)
     {
         var n = 1000;
-        var res = new float[n];
+        var res = new (float, float)[n];
         for (int i = 0; i < n; i++)
         {
-            res[i] = (float)Math.Sin(i * 2 * Math.PI / n);
+            res[i] = (i / (float)n, (float)Math.Sin(i * 2 * Math.PI / n));
         }
         return res;
     }
