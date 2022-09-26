ed612cf16e27dc121276b77e9b75173a3db45cce
Stephan Troyer <stephantroyer@live.at>
2022-09-26 18:04:03 +0200

    implement osci metadata

--- Shared/ExperimentComponents/OscilloscopeComponent.cs
+++ Shared/ExperimentComponents/OscilloscopeComponent.cs
@@ -8,12 +8,20 @@ public enum OscilloscopeTriggerMode
     Stop,
 }
 
+public struct OscilloscopeWaveform
+{
+    public (float Time, float Value)[] Data;
+    public DateTime CreationTime;
+    public (double, double)? DisplayBound;
+    public (double, double) Timespan;
+}
+
 [DisplayName("Oscilloscope")]
 [IconString("\xE9D9")]
 public abstract class OscilloscopeComponent : ExperimentComponentClass
 {
     public abstract OscilloscopeTriggerMode TriggerMode { get; set; }
-    public abstract Task<(float, float)[]> GetWaveform(string channel);
+    public abstract Task<OscilloscopeWaveform> GetWaveform(string channel);
     public abstract Task WaitForNewData(CancellationToken cancelToken = default);
 }
 
@@ -28,15 +36,21 @@ public class FakeOscilloscope : OscilloscopeComponent
 
     public override OscilloscopeTriggerMode TriggerMode { get; set; } = OscilloscopeTriggerMode.Stop;
 
-    public override Task<(float, float)[]> GetWaveform(string channel)
+    public override Task<OscilloscopeWaveform> GetWaveform(string channel)
     {
         var n = 1000;
-        var res = new (float, float)[n];
+        var data = new (float, float)[n];
         for (int i = 0; i < n; i++)
         {
-            res[i] = (i / (float)n, (float)Math.Sin(i * 2 * Math.PI / n));
+            data[i] = (i / (float)n, (float)Math.Sin(i * 2 * Math.PI / n));
         }
-        return Task.FromResult(res);
+        return Task.FromResult(new OscilloscopeWaveform
+        {
+            Data = data,
+            CreationTime = DateTime.Now,
+            DisplayBound = (-1.5, 1.5),
+            Timespan = (0, 1),
+        });
     }
 
     public override Task WaitForNewData(CancellationToken cancelToken = default)
